1c9d51ea212e698fe7bf868709c9239be80fff49
lucca ribeiro <ribeiro.boll.dev@gmail.com>
2025-09-03 19:37:33 -0300

    Ta feito, agr so falta o sql

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#include <sys/file.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -6,6 +7,8 @@
 #include <pthread.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h> 
+#include <ifaddrs.h>
 #include <ctype.h>
 #include <time.h>
 
@@ -22,10 +25,21 @@ int taskquant = 0;
 2 - css
 3 - ico
 
-
-image/x-icon
-
 */
+long int get_text_bytes(char *str){
+    return strlen(str);
+}
+
+long long int convert_int(char* nmr){
+    long long int size_of_str = 0;
+    int limit= strlen(nmr);
+    for (int i = 0;i<limit;i++){
+        if (48 <= nmr[i] && 57 >= nmr[i]){
+            size_of_str = size_of_str*10 + (nmr[i] - 48);
+        } 
+    }
+    return size_of_str;
+}
 
 char *mimeTypes[] = {"text/html","text/javascript","text/css","image/x-icon"};
 
@@ -41,6 +55,9 @@ typedef struct Task{
     int tsk_socketfd_cliente;
     int tsk_socketfd_servidor;
 
+    long int request_size_bytes;
+    char *tsk_full_request; // buffer with all request contents
+    char *tsk_headers_only;
     char *tsk_task_connection;
     char *tsk_what_frontend_wants; // to sem criatividade :)
 
@@ -51,12 +68,20 @@ typedef struct Task{
 Task *inicio = NULL;
 Task *final  = NULL;
 
-void addTask(char *typeConnection,char *what_frontend_wants,int socketfd_cliente,int socketfd_servidor){
+void addTask(long int req_size,char *full_request,char *header,char *typeConnection,char *what_frontend_wants,int socketfd_cliente,int socketfd_servidor){
+    int testes =0;
+    
     Task *newtask = malloc(sizeof(Task));
     newtask->tsk_task_connection = strdup(typeConnection);
     newtask->tsk_what_frontend_wants = strdup(what_frontend_wants);
+    
+    newtask->tsk_full_request = malloc(req_size+1);
+    memcpy(newtask->tsk_full_request, full_request, req_size);    
+
+    newtask->tsk_headers_only = strdup(header);
     newtask->tsk_socketfd_cliente = socketfd_cliente;
     newtask->tsk_socketfd_servidor = socketfd_servidor;
+    newtask->request_size_bytes = req_size;
     newtask->tsk_next = NULL;
     if (final == NULL){
         inicio = newtask;
@@ -94,7 +119,7 @@ int create_socket(int *socketfd){
         return status2;
     }
 
-    if ((status3 = listen(*socketfd, 10)) != 0){
+    if ((status3 = listen(*socketfd, 100)) != 0){
         printf("err listen\n");
         close(*socketfd);
         return status3;
@@ -139,26 +164,27 @@ void send_text(char *path,int socketfd_client, char* mimeType){
     long int bytes = get_file_bytes(path);
     snprintf(header_buffer, 4096, "%s%s; charset=utf-8\r\nAccess-Control-Allow-Origin: *\r\nContent-Length: %ld\r\n\r\n",header,mimeType,bytes);
     send(socketfd_client, header_buffer, strlen(header_buffer), 0);
+    
     //--------- file sending part ----------
-    int contador = 0;
+
+    int request_full_size = 0;
     FILE *arq = fopen(path, "r");
     char ch = 'a', file_buffer[8192];
-
     while (ch!=EOF) {
-        if (contador > 8190){
+        if (request_full_size > 8190){
             ch = getc(arq);
-            file_buffer[contador] = ch;
+            file_buffer[request_full_size] = ch;
             send(socketfd_client, file_buffer, strlen(file_buffer)-1, 0);
-            contador = 0;    
+            request_full_size = 0;    
             memset(file_buffer, 0, 8192);
         }
         else {
             ch = getc(arq);
-            file_buffer[contador] = ch;
-            contador++;
+            file_buffer[request_full_size] = ch;
+            request_full_size++;
         }
     }
-    if (contador>0){
+    if (request_full_size>0){
         send(socketfd_client, file_buffer, strlen(file_buffer)-1, 0);
     }
     close(socketfd_client);
@@ -173,31 +199,113 @@ void send_file(char *path,int socketfd_client, char *mimeType){
     snprintf(header_buffer, 4096, "%s%s; charset=utf-8\r\nAccess-Control-Allow-Origin: *\r\nContent-Length: %ld\r\n\r\n",header,mimeType,bytes);
     send(socketfd_client, header_buffer, strlen(header_buffer), 0);
     //--------- file sending part ----------
-    int contador = 0;
-    long int contador_supremo = 0;
+    int request_full_size = 0;
+    long int request_full_size_supremo = 0;
     FILE *arq = fopen(path, "rb");
     char ch = 'a', file_buffer[8192];
 
-    while (contador_supremo!=bytes) {
-        if (contador > 8190){
-            file_buffer[contador] = ch;
-            send(socketfd_client, file_buffer, contador, 0);
-            contador = 0;    
+    while (request_full_size_supremo!=bytes) {
+        if (request_full_size > 8190){
+            file_buffer[request_full_size] = ch;
+            send(socketfd_client, file_buffer, request_full_size, 0);
+            request_full_size = 0;    
         }
         else {
             ch = getc(arq);
-            file_buffer[contador] = ch;
-            contador++;
-            contador_supremo++;
+            file_buffer[request_full_size] = ch;
+            request_full_size++;
+            request_full_size_supremo++;
         }
     }
-    if (contador>0){
-        send(socketfd_client, file_buffer, contador, 0);
+    if (request_full_size>0){
+        send(socketfd_client, file_buffer, request_full_size, 0);
     }
     fclose(arq);
 }
 
+//--------------------------------------------------------
 
+
+// ----------------- processamento do upload --------------
+
+void recive_file(Task *temp){
+    int testes =0;
+    //printf("Teste %d\n",testes);testes++;
+    char *temp1 = strdup(temp->tsk_headers_only);
+    char *temp2 = strdup(temp->tsk_headers_only);
+    char *temp4 = strdup(temp->tsk_full_request);
+
+   // printf("Teste %d\n",testes);testes++;
+
+    char *file_ptr = strstr(temp->tsk_full_request, "<<<FILE_DATA_START>>>");
+
+    //printf("Teste %d\n",testes);testes++;
+    char *buffer1 = strstr(temp2, "X-File-Size: ");
+    char *buffer2 = strstr(temp2, "X-File-Name: ");
+    
+    //printf("Teste %d\n",testes);testes++;
+
+    char tempr1[8192],tempr2[8192];
+    strcpy(tempr1, buffer1);
+    strcpy(tempr2, buffer2);
+
+    //printf("Teste %d\n",testes);testes++;
+
+    char *content_lenght = strtok(tempr1, "\n");
+    char *uploaded_file_name = strtok(tempr2, "\n");
+
+    //printf("Teste %d\n",testes);testes++;
+
+    uploaded_file_name+=13;
+    uploaded_file_name[strlen(uploaded_file_name)-1] = '\0';
+
+    //printf("Teste %d\n",testes);testes++;
+
+    int header_lenght =  get_text_bytes(temp->tsk_headers_only) ;
+    //printf("Teste %d\n",testes);testes++;
+    long int file_buffer_lenght = temp->request_size_bytes - header_lenght - 39;
+    //printf("Teste %d\n",testes);testes++;
+    int true_size_file = convert_int(content_lenght);
+    //printf("Teste %d\n",testes);testes++;
+    char file_location[4096];
+    //printf("Teste %d\n",testes);testes++;
+    strcpy(file_location, uploaded_file_name);
+    //printf("Teste %d\n",testes);testes++;
+    char *tempptr = strstr(file_location,".");
+
+    
+
+    tempptr[0] = '\0';
+    mkdir(uploaded_file_name,0755);
+    snprintf(file_location, 4096, "%s/%s", uploaded_file_name,uploaded_file_name);
+
+    FILE *arq = fopen(file_location, "wb");
+    int arquivofd = fileno(arq);
+    flock(arquivofd, LOCK_EX);
+    //printf("%s\n",temp->tsk_headers_only);
+    file_ptr+=22;
+    fwrite(file_ptr, 1, file_buffer_lenght, arq);
+    if ((temp->request_size_bytes - header_lenght - 39)  < true_size_file) {
+        printf("Starting Big Upload...\n");
+        while ((temp->request_size_bytes - header_lenght)  < true_size_file) {
+            char buffer_socket[8192];
+            int recv_size = recv(temp->tsk_socketfd_cliente, buffer_socket, 8192, 0);
+            if (recv_size == 0) break;
+            temp->request_size_bytes += recv_size;
+            fwrite(buffer_socket, 1, recv_size, arq);
+        }
+        printf("Upload finished! :D\n");
+    }
+    fclose(arq);
+    flock(arquivofd, LOCK_UN);
+    send(temp->tsk_socketfd_cliente, "HTTP/1.1 200 OK", 16, 0);
+    //printf("-------------------------pinto gay inicio-------------------------\n%s\n-------------------------pinto gay final----------
+    // ---------------\n%ld\n%ld\n",temp->tsk_headers_only,get_text_bytes(temp->tsk_headers_only),get_text_bytes(full_content_lenght));
+    close(temp->tsk_socketfd_cliente);
+    free(temp1);
+    free(temp2);
+    free(temp4);
+}
 
 // -------------------------------------------------------
 
@@ -208,19 +316,19 @@ void *routine(void* arg){
             pthread_cond_wait(&tsk_thcond, &tsk_mutex);
         }
         Task *temp = inicio;
-        printf("[Request type: %s][Requested: %s][SocketFD: %d]\n",temp->tsk_task_connection, temp->tsk_what_frontend_wants,temp->tsk_socketfd_cliente);
+        printf("[Request type: %s][Requested: %s][SocketFD: %d]\n\n",temp->tsk_task_connection, temp->tsk_what_frontend_wants,temp->tsk_socketfd_cliente);
         taskquant--;
 
         if (inicio->tsk_next == NULL){
             inicio  = NULL;
             final = NULL;
         }
+
         else {
             inicio = inicio->tsk_next;
         }
-        
-        
-        pthread_mutex_unlock(&tsk_mutex); 
+        pthread_mutex_unlock(&tsk_mutex);
+        // -------------  processamento de requisiçoes------------------
         if (strcmp(temp->tsk_task_connection, "GET") == 0){
             if (strcmp(temp->tsk_what_frontend_wants,"/") == 0){
                 send_text("www/index.html", temp->tsk_socketfd_cliente,mimeTypes[0]);
@@ -244,33 +352,32 @@ void *routine(void* arg){
         }
 
         else if (strcmp(temp->tsk_task_connection, "POST") == 0){
-
+            if (strcmp(temp->tsk_what_frontend_wants,"/upload") == 0){
+                recive_file(temp);
+            }
         }
+
+        else if (strcmp(temp->tsk_task_connection, "OPTIONS") == 0) {
+            char buffer[4096];
+            snprintf(buffer, 4096, "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: POST, OPT\r\nIONSAccess-Control-Allow-Headers: Content-Type\r\nContent-Length: 0");
+            send(temp->tsk_socketfd_cliente, buffer, strlen(buffer)-1, 0);
+            close(temp->tsk_socketfd_cliente);
+        }
+        //--------------------------------------
         free(temp->tsk_task_connection);
         free(temp->tsk_what_frontend_wants);
+        free(temp->tsk_full_request);
         free(temp);
         pthread_cond_broadcast(&tsk_thcond);
-        //
-        // free(temp);
     }
 }
+
+//-----------------------------------------------
 
 /*
 
 
 
-
-GET /favicon.ico HTTP/1.1
-Host: 127.0.0.1:8080
-User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:142.0) Gecko/20100101 Firefox/142.0
-Accept: image/avif,image/webp,image/png,image/svg+xml,image/;q=0.8,;q=0.5
-Accept-Language: en-US,en;q=0.5
-Accept-Encoding: gzip, deflate, br, zstd
-Connection: keep-alive
-Referer: http://127.0.0.1:8080/
-Sec-Fetch-Dest: image
-Sec-Fetch-Mode: no-cors
-Sec-Fetch-Site: same-origin
 
 1 - html
 
@@ -278,6 +385,32 @@ Sec-Fetch-Site: same-origin
 
 */
 
+void setIp(char *port){
+    struct ifaddrs *ifaddr, *ifa;
+    int family, s;
+    char host[NI_MAXHOST];
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        family = ifa->ifa_addr->sa_family;
+
+        if (family == AF_INET) {
+            s = getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in),host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+            if (s != 0) {
+                printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+            }
+            else if (strcmp(ifa->ifa_name, "wlp3s0")==0 || strcmp(ifa->ifa_name, "wlan0") == 0 || strcmp(ifa->ifa_name, "enp0s25")==0) {
+                netinfo.addr = strdup(host);
+                netinfo.port = strdup(port);
+            }
+        }
+    }
+}
 
 int main(){
     pthread_mutex_init(&tsk_mutex, NULL);
@@ -286,14 +419,13 @@ int main(){
 
     int socketfd,socket_clientfd;
 
-    netinfo.addr = "127.0.0.1";
-    netinfo.port = "8080";
+    setIp("9988");
 
     struct sockaddr_storage client_conf;
     memset(&client_conf, 0, sizeof(client_conf));
     socklen_t size = sizeof(client_conf);
     int status = create_socket(&socketfd);
-    char buffer[4096], *clone,*tipo_conexao,*nome_arquivo;
+    char buffer[8192],clone_buffer[8192], *headers,*tipo_conexao,*nome_arquivo;
     printf("http://%s:%s\n\n",netinfo.addr,netinfo.port);
 
     
@@ -303,21 +435,26 @@ int main(){
 
     while (1) {
         socket_clientfd = accept(socketfd, (struct sockaddr *)&client_conf, &size);
-        recv(socket_clientfd,buffer, 4096, 0);
-        clone = strdup(buffer);
+        int req_size = recv(socket_clientfd,buffer, 8192, 0);
+        strcpy(clone_buffer, buffer);
         
+        headers = strdup("temporario...");
+
         tipo_conexao = strdup(http_parser_of_type_request(buffer));
         nome_arquivo = strdup(http_parser_of_what_do_frontend_wants(buffer));
-        
-        addTask(tipo_conexao, nome_arquivo, socket_clientfd,socketfd);
+        if (strcmp(nome_arquivo, "/upload") == 0){
+            headers = strstr(clone_buffer, "<<<HEADER_END>>>");
+            headers[0] = '\0';
+            //printf("%s",buffer);
+
+        }
+        pthread_mutex_lock(&tsk_mutex);
+        addTask(req_size,buffer,clone_buffer,tipo_conexao, nome_arquivo, socket_clientfd,socketfd);
         taskquant++;
-        printf("-----------------------------\n%s\n-------------------------------\n\n",buffer);
+        pthread_mutex_unlock(&tsk_mutex);
         pthread_cond_signal(&tsk_thcond);
     }
-    getc(stdin);
 }
-
-
 
 // GET / HTTP/1.1
 // Host: 127.0.0.1:8000
--- www/script.js
+++ www/script.js
@@ -1,7 +1,6 @@
 let selectedFile = null;
 let clientIp = 'Detectando...';
 
-// Detectar IP do cliente
 async function detectClientIp() {
     try {
         const response = await fetch('https://api.ipify.org?format=json');
@@ -13,13 +12,27 @@ async function detectClientIp() {
         document.getElementById('clientIp').textContent = clientIp;
     }
 }
+function autoDetectServer() {
+    const serverUrlField = document.getElementById('serverUrl');
+    
+    if (serverUrlField.value && 
+        !serverUrlField.value.includes('localhost') && 
+        !serverUrlField.value.includes('127.0.0.1') &&
+        serverUrlField.value.trim() !== '') {
+        console.log('🎯 URL do servidor já configurada:', serverUrlField.value);
+        return;
+    }
+    
+    const currentOrigin = window.location.origin;
+    serverUrlField.value = currentOrigin;
+    console.log('🎯 URL do servidor auto-detectada:', currentOrigin);
+    showStatus(`Servidor detectado automaticamente: ${currentOrigin}`, 'success');
+}
 
-// Gerar ID único
 function generateUniqueId() {
     return 'file_' + Date.now() + '_' + Math.random().toString(36).substr(2, 9);
 }
 
-// Formatar tamanho do arquivo
 function formatFileSize(bytes) {
     if (bytes === 0) return '0 Bytes';
     const k = 1024;
@@ -28,7 +41,6 @@ function formatFileSize(bytes) {
     return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 }
 
-// Configurar eventos de drag and drop
 const uploadArea = document.getElementById('uploadArea');
 const fileInput = document.getElementById('fileInput');
 
@@ -60,57 +72,52 @@ fileInput.addEventListener('change', (e) => {
     }
 });
 
-// Manipular seleção de arquivo
 function handleFileSelection(file) {
     selectedFile = file;
     const fileId = generateUniqueId();
     const uploadTime = new Date().toLocaleString('pt-BR');
-
     document.getElementById('fileName').textContent = file.name;
     document.getElementById('fileSize').textContent = formatFileSize(file.size);
     document.getElementById('fileType').textContent = file.type || 'Desconhecido';
     document.getElementById('fileId').textContent = fileId;
     document.getElementById('uploadTime').textContent = uploadTime;
-
     document.getElementById('fileInfo').classList.add('show');
 }
 
-// Função de upload
+// VERSÃO CUSTOMIZADA PARA BACKEND C
 async function uploadFile() {
     if (!selectedFile) {
         showStatus('Selecione um arquivo primeiro!', 'error');
         return;
     }
 
-    const serverUrl = document.getElementById('serverUrl').value;
+    const serverUrl = document.getElementById('serverUrl').value.trim();
+    console.log('=== DEBUG UPLOAD ===');
+    console.log('URL configurada:', serverUrl);
+    console.log('URL final para upload:', `${serverUrl}/upload`);
+
+    if (!serverUrl) {
+        showStatus('URL do servidor não detectada! Recarregue a página.', 'error');
+        return;
+    }
+
     const progressBar = document.getElementById('progressBar');
     const progressFill = document.getElementById('progressFill');
     const uploadBtn = document.getElementById('uploadBtn');
-
-    // Mostrar barra de progresso
+    
     progressBar.style.display = 'block';
     uploadBtn.disabled = true;
     uploadBtn.textContent = 'Enviando...';
 
     try {
-        // Criar FormData com as informações
-        const formData = new FormData();
-        formData.append('file', selectedFile);
-        formData.append('fileId', document.getElementById('fileId').textContent);
-        formData.append('clientIp', clientIp);
-        formData.append('uploadTime', document.getElementById('uploadTime').textContent);
-        formData.append('originalSize', selectedFile.size.toString());
-
-        // Criar XMLHttpRequest para ter controle do progress
+        const customRequest = await createCustomHttpRequest(selectedFile);
         const xhr = new XMLHttpRequest();
-
         xhr.upload.addEventListener('progress', (e) => {
             if (e.lengthComputable) {
                 const percentComplete = (e.loaded / e.total) * 100;
                 progressFill.style.width = percentComplete + '%';
             }
         });
-
         xhr.addEventListener('load', () => {
             if (xhr.status === 200) {
                 showStatus('Arquivo enviado com sucesso! 🎉', 'success');
@@ -120,87 +127,81 @@ async function uploadFile() {
             }
             resetUploadState();
         });
-
         xhr.addEventListener('error', () => {
             showStatus('Erro de conexão com o servidor', 'error');
             resetUploadState();
         });
-
         xhr.open('POST', `${serverUrl}/upload`);
-        xhr.send(formData);
+        xhr.setRequestHeader('Content-Type', 'application/octet-stream');
+        xhr.setRequestHeader('X-File-Name', selectedFile.name);
+        xhr.setRequestHeader('X-File-Size', selectedFile.size.toString());
+        xhr.setRequestHeader('X-File-Id', document.getElementById('fileId').textContent);
+        xhr.setRequestHeader('X-Client-IP', clientIp);
+        xhr.setRequestHeader('X-Upload-Time', document.getElementById('uploadTime').textContent);
+        
+        xhr.send(customRequest);
 
     } catch (error) {
         showStatus(`Erro: ${error.message}`, 'error');
         resetUploadState();
     }
 }
-
+async function createCustomHttpRequest(file) {
+    const fileId = document.getElementById('fileId').textContent;
+    const uploadTime = document.getElementById('uploadTime').textContent;
+    const header = 
+        `<<<HEADER_START>>>\n` +
+        `FILENAME: ${file.name}\n` +
+        `FILESIZE: ${file.size}\n` +
+        `FILETYPE: ${file.type || 'application/octet-stream'}\n` +
+        `FILEID: ${fileId}\n` +
+        `CLIENTIP: ${clientIp}\n` +
+        `UPLOADTIME: ${uploadTime}\n` +
+        `<<<HEADER_END>>>\n` +
+        `<<<FILE_DATA_START>>>\n`;
+    const footer = ``;
+    const fileBuffer = await file.arrayBuffer();
+    const headerBytes = new TextEncoder().encode(header);
+    const footerBytes = new TextEncoder().encode(footer);  
+    const totalSize = headerBytes.length + fileBuffer.byteLength + footerBytes.length;
+    const combinedBuffer = new ArrayBuffer(totalSize);
+    const combinedView = new Uint8Array(combinedBuffer);
+    combinedView.set(headerBytes, 0);
+    combinedView.set(new Uint8Array(fileBuffer), headerBytes.length);
+    combinedView.set(footerBytes, headerBytes.length + fileBuffer.byteLength);
+    
+    return combinedBuffer;
+}
 function resetUploadState() {
     document.getElementById('progressBar').style.display = 'none';
     document.getElementById('progressFill').style.width = '0%';
     document.getElementById('uploadBtn').disabled = false;
     document.getElementById('uploadBtn').textContent = '🚀 Fazer Upload';
 }
-
 function resetForm() {
     selectedFile = null;
     document.getElementById('fileInput').value = '';
     document.getElementById('fileInfo').classList.remove('show');
 }
-
 function showStatus(message, type) {
     const status = document.getElementById('status');
     status.textContent = message;
     status.className = `status ${type}`;
     status.style.display = 'block';
-
     setTimeout(() => {
         status.style.display = 'none';
     }, 5000);
 }
-
-// Inicializar
-detectClientIp();
-
-// Exemplo de como fazer request raw HTTP/1.1 (para referência)
-function createRawHttpRequest(file, metadata) {
-    const boundary = '----WebKitFormBoundary' + Math.random().toString(36).substr(2);
-    const CRLF = '\r\n';
-
-    let body = '';
-
-    // Adicionar metadados
-    body += `--${boundary}${CRLF}`;
-    body += `Content-Disposition: form-data; name="fileId"${CRLF}${CRLF}`;
-    body += `${metadata.fileId}${CRLF}`;
-
-    body += `--${boundary}${CRLF}`;
-    body += `Content-Disposition: form-data; name="clientIp"${CRLF}${CRLF}`;
-    body += `${metadata.clientIp}${CRLF}`;
-
-    body += `--${boundary}${CRLF}`;
-    body += `Content-Disposition: form-data; name="uploadTime"${CRLF}${CRLF}`;
-    body += `${metadata.uploadTime}${CRLF}`;
-
-    body += `--${boundary}${CRLF}`;
-    body += `Content-Disposition: form-data; name="originalSize"${CRLF}${CRLF}`;
-    body += `${metadata.originalSize}${CRLF}`;
-
-    // Adicionar arquivo
-    body += `--${boundary}${CRLF}`;
-    body += `Content-Disposition: form-data; name="file"; filename="${file.name}"${CRLF}`;
-    body += `Content-Type: ${file.type || 'application/octet-stream'}${CRLF}${CRLF}`;
-    // Aqui viria o conteúdo binário do arquivo
-    body += `${CRLF}--${boundary}--${CRLF}`;
-
-    return {
-        method: 'POST',
-        headers: {
-            'Content-Type': `multipart/form-data; boundary=${boundary}`,
-            'Content-Length': body.length.toString(),
-            'Host': '127.0.0.1:8080',
-            'User-Agent': 'FileUploadClient/1.0'
-        },
-        body: body
-    };
+document.addEventListener('DOMContentLoaded', () => {
+    detectClientIp();
+    autoDetectServer();
+});
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => {
+        detectClientIp();
+        autoDetectServer();
+    });
+} else {
+    detectClientIp();
+    autoDetectServer();
 }
